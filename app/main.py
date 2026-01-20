a34fcfafc2fa441890737905860a61162d16b15b
Oleksandr Simochko <vhsb721@gmail.com>
2026-01-20 20:11:05 +0100

    Solution

--- app/main.py
+++ app/main.py
@@ -25,10 +25,10 @@ class Distance:
         return self
 
     def __mul__(self, other: int | float) -> Distance:
-            return Distance(self.km * other)
+        return Distance(self.km * other)
 
     def __truediv__(self, other: int | float) -> Distance:
-            return Distance(round(self.km / other, 2))
+        return Distance(round(self.km / other, 2))
 
     def __lt__(self, other: Distance | int | float) -> bool:
         if isinstance(other, Distance):
